9001d400608f59affe44968d30626e389aee74e3
omobolaji29 <omobolagee29@gmail.com>
2022-10-09 18:53:16 +0100

    changes

--- bola.js
+++ bola.js
@@ -1,1 +1,15 @@
-hjdcbhjcjkdbhjjhbd 
+const Sidebar = () => {
+  
+    return (
+      <div> 
+    
+     <div className="navbarr">
+       <div className="navbar-color">
+          <img src={food} alt ="food logo"/><br/>
+          <h1> Bola's Cafe</h1></div>
+    
+          <img src={dashimg} alt= 'dashimg' className='dashimg'/>
+     
+      </div> 
+      </div>
+      )}
